f77792d7959e2d678f2911f60c1128c3670dac27
muhammadwali0 <waleeraxa@gmail.com>
2025-04-26 13:58:37 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+def print_logo():
+    # Placeholder for your ASCII logo
+    # Add your ASCII art logo here
+    print("""
+
+      _     _____      _  ___       _       _ 
+  ___| |__ |___ / _ __| |/ _ \  ___| | ____| |
+ / __| '_ \  |_ \| '__| | | | |/ __| |/ / _` |
+ \__ \ | | |___) | |  | | |_| | (__|   < (_| |
+ |___/_| |_|____/|_|  |_|\___/ \___|_|\_\__,_|
+                                              
+
+
+
+    """)
+
+def main():
+    print_logo()
+    print("\nWelcome to sh3rl0ckd - Information Gathering Toolkit\n")
+    print("This is the main menu where options will be displayed.")
+    print("More features coming soon...")
+
+if __name__ == '__main__':
+    main()
